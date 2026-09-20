14878eb0c8954461062699d68448660d57becfa4
Dmitry <casyyycas.yyy@gmail.com>
2026-09-20 23:48:58 +0300

    feat: initialize Spring Boot project

---
+++ src/main/java/com/example/pmsbooking/PmsBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.pmsbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmsBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pmsbooking/PmsBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pmsbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
